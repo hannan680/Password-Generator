9baad4698fad214123161c7999ebdab5f0800697
hannan680 <hannanshoukat55@gmail.com>
2024-05-07 21:34:52 +0500

    initial commit

---
+++ js/password.js
@@ -0,0 +1,69 @@
+const lengthSlider = document.querySelector(".pass-length input"),
+  options = document.querySelectorAll(".option input"),
+  copyIcon = document.querySelector(".input-box span"),
+  passwordInput = document.querySelector(".input-box input"),
+  passIndicator = document.querySelector(".pass-indicator"),
+  generateBtn = document.querySelector(".generate-btn");
+
+const characters = {
+  lowercase: "abcdefghijklmnopqrstuvwxyz",
+  uppercase: "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+  numbers: "0123456789",
+  symbols: "^!$%&|[](){}:;.,*+-#@<>~",
+};
+
+const generatePassword = async () => {
+  const length = lengthSlider.value;
+  const options = Array.from(
+    document.querySelectorAll(".option input:checked")
+  ).map((input) => input.id);
+  try {
+    const response = await fetch(
+      "https://amused-deer-handbag.cyclic.app/generate-password",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ length, options }),
+      }
+    );
+    if (!response.ok) {
+      throw new Error("Failed to generate password");
+    }
+    const data = await response.json();
+    passwordInput.value = data.password;
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+const updatePassIndicator = () => {
+  passIndicator.id =
+    lengthSlider.value <= 8
+      ? "weak"
+      : lengthSlider.value <= 16
+      ? "medium"
+      : "strong";
+};
+
+const updateSlider = () => {
+  document.querySelector(".pass-length span").innerText = lengthSlider.value;
+  generatePassword();
+  updatePassIndicator();
+};
+updateSlider();
+
+const copyPassword = () => {
+  navigator.clipboard.writeText(passwordInput.value);
+  copyIcon.innerText = "check";
+  copyIcon.style.color = "#4285F4";
+  setTimeout(() => {
+    copyIcon.innerText = "copy_all";
+    copyIcon.style.color = "#fff";
+  }, 1500);
+};
+
+copyIcon.addEventListener("click", copyPassword);
+lengthSlider.addEventListener("input", updateSlider);
+generateBtn.addEventListener("click", generatePassword);
